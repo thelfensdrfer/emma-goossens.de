03d35239fadf42dc32d6692b0cba7271690f9055
Tim Helfensdörfer <thelfensdrfer@gmail.com>
2023-11-20 12:24:10 +0100

    feat: add canonical urls

--- src/gatsby-config.js
+++ src/gatsby-config.js
@@ -29,5 +29,10 @@ module.exports = {
                 domain: `emma-goossens.de`,
                 customDomain: `stats.codigy.de/js/script.js?original=`,
             },
+        }, {
+            resolve: `gatsby-plugin-canonical-urls`,
+            options: {
+                siteUrl: `https://www.emma-goossens.de`,
+            },
         }]
 }
